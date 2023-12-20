08c1c2268fffb2e9b17ff522b9ee8238e8fd3067
Csaba Valyi <csaba.valyi1@gmail.com>
2023-12-20 10:00:13 +0100

    remove console.log

--- backend/src/util/movide-db-poster-url-maker.ts
+++ backend/src/util/movide-db-poster-url-maker.ts
@@ -1,5 +1,4 @@
 import { THEMOVIEDB_IMAGE_URL } from '@config'
-console.log('asdasd', THEMOVIEDB_IMAGE_URL)
 
 const createPosterUrl = (postfix: string) => {
   return `${THEMOVIEDB_IMAGE_URL}/w92/${postfix}`
