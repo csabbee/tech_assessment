beec2cb75320c994d42456afb55e285e11ea3a4e
Csaba Valyi <csaba.valyi1@gmail.com>
2023-12-21 11:58:08 +0100

    create proxy for the dev environment

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 
+export const NDOE_ENV = process.env.NODE_ENV || 'dev'
 export const PORT = process.env.PORT || 3000
 export const THEMOVIEDB_API_KEY = process.env.THEMOVIEDB_API_KEY || 'testing'
 export const THEMOVIEDB_URL = process.env.THEMOVIEDB_URL || 'https://api.themoviedb.org/3'
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import { PORT } from '@config'
+import { NDOE_ENV, PORT } from '@config'
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 import routes from './routes'
 
@@ -8,6 +9,16 @@ const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(routes)
+
+if (NDOE_ENV === 'dev') {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'http://localhost:5173',
+      changeOrigin: true,
+    })
+  )
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
